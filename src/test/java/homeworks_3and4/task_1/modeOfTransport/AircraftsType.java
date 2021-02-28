5ade5c9548e3d8dbcf0d26a5f4a2358e20105695
Elena <ebogacheva@yahoo.com>
2021-03-01 00:25:17 +0300

    Revert "Revert "homework3_task1""
    This reverts commit 2c81587dfc702c316f8545e02aa4cc928eaa40ff.

---
+++ src/test/java/homeworks_3and4/task_1/modeOfTransport/AircraftsType.java
@@ -0,0 +1,9 @@
+package homeworks_3and4.task_1.modeOfTransport;
+
+public enum AircraftsType {
+    PLANE,
+    HELICOPTER,
+    MULTICOPTER,
+    AIRSHIP,
+    GAS_BALLON;
+}
---
+++ src/test/java/homeworks_3and4/task_1/modeOfTransport/BoeingType.java
@@ -0,0 +1,8 @@
+package homeworks_3and4.task_1.modeOfTransport;
+
+public enum BoeingType {
+    BOEING_737,
+    BOEING_747,
+    BOEING_767,
+    BOEING_777;
+}
